e136e2b77dda0f08d446a9fc931468dee7ddfdf2
noname-new <huylim2013@gmail.com>
2026-08-23 22:10:58 +0700

    Update main.lua

--- scriptRBL/Jujutsu_shenanigans/main.lua
+++ scriptRBL/Jujutsu_shenanigans/main.lua
@@ -1,2 +1,5 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/script%20JJS%20kill%20all%20gojo%2C%20bam%20nguoi.lua"))()
+
+-- JujutsuerV2 hub
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/solarastuff/tze/refs/heads/main/JujutsuerV2.lua"))()
