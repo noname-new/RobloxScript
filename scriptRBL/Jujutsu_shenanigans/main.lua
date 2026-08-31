b28620d7cd37270e4551ead558dbb6727ae36a96
noname-new <huylim2013@gmail.com>
2026-08-31 16:06:16 +0700

    Fix loading of JujutsuerV2 hub script

--- scriptRBL/Jujutsu_shenanigans/main.lua
+++ scriptRBL/Jujutsu_shenanigans/main.lua
@@ -1,5 +1,8 @@
+--TBO hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"))()
+
+--script bám người
 loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/script%20JJS%20kill%20all%20gojo%2C%20bam%20nguoi.lua"))()
 
 -- JujutsuerV2 hub
---loadstring(game:HttpGet("https://raw.githubusercontent.com/solarastuff/tze/refs/heads/main/JujutsuerV2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/solarastuff/tze/refs/heads/main/JujutsuerV2.lua"))()
