173d02e4ba4593bb9f949dd7eda9ac74d1910213
noname-new <huylim2013@gmail.com>
2026-08-31 19:26:33 +0700

    Refactor main.lua for Jujutsu shenanigans hub

--- scriptRBL/Jujutsu_shenanigans/main.lua
+++ scriptRBL/Jujutsu_shenanigans/main.lua
@@ -1,8 +1,14 @@
---TBO hub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"))()
-
---script bám người
-loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/script%20JJS%20kill%20all%20gojo%2C%20bam%20nguoi.lua"))()
-
 -- JujutsuerV2 hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/solarastuff/tze/refs/heads/main/JujutsuerV2.lua"))()
+
+-- Chờ 10 giây
+task.wait(10)
+
+-- TBO hub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"))()
+
+-- Chờ 5 giây
+task.wait(5)
+
+-- Script bám người
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/script%20JJS%20kill%20all%20gojo%2C%20bam%20nguoi.lua"))()
