1bf3a958ef31eabcc6912f4f267999e66f310568
noname-new <huylim2013@gmail.com>
2026-09-03 22:25:55 +0700

    Update script loading sequence in main.lua
    Reordered script loading sequence and added wait times.

--- scriptRBL/Jujutsu_shenanigans/main.lua
+++ scriptRBL/Jujutsu_shenanigans/main.lua
@@ -1,8 +1,10 @@
--- JujutsuerV2 hub
-loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/jujutsuerV2.lua"))()
-
--- TBO hub
+-- TBO hub (LOAD TRƯỚC)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/cool5013/TBO/main/TBOscript"))()
+wait(1) -- Chờ 1 giây
 
--- Script bám người
+-- Script bám người (LOAD TRƯỚC)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/script%20JJS%20kill%20all%20gojo%2C%20bam%20nguoi.lua"))()
+wait(10) -- Chờ 10 giây
+
+-- JujutsuerV2 hub (LOAD SAU CÙNG)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/noname-new/RobloxScript/refs/heads/main/scriptRBL/Jujutsu_shenanigans/jujutsuerV2.lua"))()
