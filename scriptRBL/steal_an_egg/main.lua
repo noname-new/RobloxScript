2be9a637a51674ae37ba72db9fa373399fc7a9a7
noname-new <huylim2013@gmail.com>
2026-09-10 23:03:22 +0700

    Update script to use Zerion Hub loader
    Comment out the Blyxo Hub loader and add the Zerion Hub loader.

--- scriptRBL/steal_an_egg/main.lua
+++ scriptRBL/steal_an_egg/main.lua
@@ -1,2 +1,5 @@
 -- Blyxo Hub – Steal An Egg Script
-loadstring(game:HttpGet("https://flowauth.net/v1/loaders/69d3463240384f3a73fbe32c178093a2.lua"))()
+--loadstring(game:HttpGet("https://flowauth.net/v1/loaders/69d3463240384f3a73fbe32c178093a2.lua"))()
+
+--zerion Hub
+loadstring(game:HttpGet("https://zeroinhub.com/api/script"))()
