95f9afa3da4bdf636d102db2de76ee83d7bbd1c6
noname-new <huylim2013@gmail.com>
2026-09-16 23:40:09 +0700

    Update main.lua

--- scriptRBL/The_strongest_Battlegrounds/main.lua
+++ scriptRBL/The_strongest_Battlegrounds/main.lua
@@ -1,1 +1,5 @@
+--vexon hub
 loadstring(game:HttpGet("https://raw.githubusercontent.com/DiosDi/VexonHub/refs/heads/main/VexonHub"))()
+
+--auto farm
+loadstring(game:HttpGet("https://gitlab.com/zkay404-group/ProjectYielding/-/raw/main/ZKPublicFarm"))()
